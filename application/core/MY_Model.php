90fb49d14eba003e4d85dd0637b9fd6375f11921
Nic Rosental <nic@epiclabs.com>
2011-10-28 13:39:46 -0400

    added support for multiple IDs in the get method of MY_Model

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -1,71 +1,102 @@
-<?php
-if (!defined('BASEPATH'))
-	exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class MY_Model extends CI_Model
-{
-
-	protected $tbl = NULL;
-
-	/**
-	 * Constructor
-	 */
-	function __construct()
-	{
-		parent::__construct();
-	}
-
-	/**
-	 * Insert
-	 */
-	function insert(array $insert_data)
-	{
-		return $this -> db -> insert($this -> tbl, $insert_data);
-	}
-
-	/**
-	 * Get all
-	 */
-	function get_all()
-	{
-
-		return $this -> db -> get($this -> tbl) -> result();
-
-	}
-
-	/**
-	 * Get By Id
-	 */
-	function get($id)
-	{
-		return $this -> db -> get_where($this -> tbl, array('id' => $id)) -> result();
-	}
-
-	/**
-	 * Get By Field
-	 * 
-	 * @todo Add options for limit and offset
-	 */
-	function get_by_field($field_name, $field_value)
-	{
-		return $this -> db -> get_where($this -> tbl, array($field_name => $field_value)) -> result();
-	}
-
-	/**
-	 * Update
-	 */
-	function update($id, array $update_data)
-	{
-		return $this -> db -> where('id', $id) -> update($this -> tbl, $update_data);
-	}
-
-	/**
-	 * Delete
-	 */
-	function delete($id)
-	{
-		return $this -> db -> where('id', $id) -> delete($this -> tbl);
-	}
-
-}
+	class MY_Model extends CI_Model {
+	
+		protected $tbl = NULL;
+		
+		/*********************************************************************************************************************/
+		
+		// Constructor
+		
+		/*********************************************************************************************************************/
+		
+		function __construct(){
+		
+	        // Call the Model constructor
+	        parent::__construct();
+	    
+	    }
+	    
+	    /*********************************************************************************************************************/
+	    
+	    // New record
+	    
+	    /*********************************************************************************************************************/
+	    
+	    function insert( array $insert_data ){
+	    
+	    	return $this->db->insert( $this->tbl, $insert_data );
+	    
+	    }
+	    
+	    /*********************************************************************************************************************/
+	    
+	    // Get all
+	    
+	    /*********************************************************************************************************************/
+	    
+	    function get_all(){
+	    
+	    	return $this->db->get( $this->tbl )->result();
+	    
+	    }
+	    
+	    /*********************************************************************************************************************/
+	    
+	    // Get by ID(s)
+	    
+	    /*********************************************************************************************************************/
+	    
+	    function get( $id ){
+	    	
+	    	if( is_array( $id ) )
+			{
+			
+				return $this->db->where_in( 'id', $id )->get( $this->tbl )->result();
+			
+			}
+	    
+	    	return $this->db->get_where( $this->tbl, array( 'id' => $id) )->result();	    
+	    
+	    }
+	    
+	    /*********************************************************************************************************************/
+	    
+	    // Get by specific criteria
+	    
+	    /*********************************************************************************************************************/
+	    
+	    function get_by_field( $field_name, $field_value ){
+	    
+	    	return $this->db->get_where( $this->tbl, array( $field_name, $field_value ) )->result();
+	    
+	    }
+	    
+	    /*********************************************************************************************************************/
+	    
+	    // Update record
+	    
+	    /*********************************************************************************************************************/
+	    
+	    function update( $id, array $update_data ){
+	    
+	    	return $this->db->where( 'id', $id )->update( $this->tbl, $update_data );
+	    
+	    }
+	    
+	    /*********************************************************************************************************************/
+	    
+	    // Delete record
+	    
+	    /*********************************************************************************************************************/
+	    
+	    function delete( $id ){
+	    
+	    	return $this->db->where( 'id', $id)->delete( $this->tbl );
+	    
+	    }
+	    
+		
+	}	
+	
 ?>
